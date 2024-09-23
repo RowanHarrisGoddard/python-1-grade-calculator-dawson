55bdfc9b685e612099761b76e31ebd597f7bc800
= <=>
2024-09-23 15:45:18 -0400

    start of assigment

--- Assignment1.py
+++ Assignment1.py
@@ -1,2 +1,12 @@
-print("hello world")
+num_of_labs= input("Enter the number of labs completed:")
+num_of_quizzes= input("Enter the number of labs completed:")
+ass_1= input("Enter grade for assignment 1:")
+ass_2= input("Enter grade for assignment 2:")
+ass_3= input("Enter grade for assignment 3:")
+ass_4= input("Enter grade for assignement 4:")
+mid_grade1= input("Enter grade for midterm 1:")
+mid_grade2= input("Enter grade for midterm 2:")
+final_grade= input("Enter grade for final exam:")
+mid_final_prep= input("Enter grade for final exam:")
 
+            
